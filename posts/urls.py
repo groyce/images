1db7a51fc170c98d38ad34b492e376a5f6cac2e5
groyce <groyce@unomaha.edu>
2019-10-14 19:26:44 -0500

    first commit

---
+++ posts/urls.py
@@ -0,0 +1,10 @@
+# posts/urls.py
+from django.urls import path
+
+from .views import HomePageView, CreatePostView
+
+urlpatterns = [
+    path('', HomePageView.as_view(), name='home'),
+    path('post/', CreatePostView.as_view(), name='add_post')
+]
+
